2810e74234fe49d4a89c65ad326cb49098892bf8
James Fitzpatrick <james.fitz123@gmail.com>
2022-11-08 10:53:16 +0000

    Add code for checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,22 @@ function runGame(gameType) {
     }
 }
 
-function checkAnswer() {
+/** 
+ * Gets the operand (numbers) and operator (plus, minus etc...)
+ * directly from the DOM and returns the correct answer
+ */
 
+function checkAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1, operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`)
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function calculateCorrectAnswer() {
@@ -53,7 +67,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
